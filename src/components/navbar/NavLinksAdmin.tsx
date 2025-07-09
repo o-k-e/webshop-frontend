c6dc5230bcdb4a0d9f3436f9c78f0726d3b218fb
o-k-e <123556323+o-k-e@users.noreply.github.com>
2025-07-10 00:21:34 +0200

    feat: add navbar styling

--- src/components/navbar/NavLinksAdmin.tsx
+++ src/components/navbar/NavLinksAdmin.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import backgroundImage from '../../assets/background-7.png';
 
 const NavLinksAdmin = () => {
 	const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -14,21 +15,33 @@ const NavLinksAdmin = () => {
 	];
 
 	return (
-		<nav>
-			<ul className="flex gap-4">
-				{links.map((link) => (
-					<li key={link.to}>
-						<NavLink
-							to={link.to}
-							className={({ isActive }) =>
-								isActive ? 'text-orange-400 font-bold' : 'text-gray-700'
-							}
-						>
-							{link.label}
-						</NavLink>
-					</li>
-				))}
-			</ul>
+		<nav className="h-full flex items-center justify-end">
+			<div className="h-full">
+				<ul
+					className="flex gap-6 h-full items-center pl-6 pr-0"
+					style={{
+						backgroundImage: `url(${backgroundImage})`,
+						backgroundRepeat: 'no-repeat',
+						backgroundSize: '100% 100%',
+						backgroundPosition: 'center center',
+						minWidth: '500px',
+						height: '100%',
+					}}
+				>
+					{links.map((link) => (
+						<li key={link.to}>
+							<NavLink
+								to={link.to}
+								className={({ isActive }) =>
+									isActive ? 'text-gold font-bold' : 'text-gold'
+								}
+							>
+								{link.label}
+							</NavLink>
+						</li>
+					))}
+				</ul>
+			</div>
 		</nav>
 	);
 };
--- src/components/navbar/NavbarUser.tsx
+++ src/components/navbar/NavbarUser.tsx
@@ -1,14 +1,31 @@
-import NavLinksUser from "./NavLinksUser"
+import NavLinksUser from './NavLinksUser';
+import ganeshaLogo from '../../assets/ganesha-logo.png';
 
 const NavbarUser = () => {
-  return (
-    <header className="p-4 shadow-md bg-white">
-      <div className="container mix-auto flex justify-between items-center">
-        <h1 className="text-xl font-semibold">🪷 Ganesha Webshop 🪷</h1>
-        <NavLinksUser />
+	return (
+    <div className="flex items-center h-full w-full">
+    {/* Bal oldal: Ganesha Admin */}
+    <div className="basis-1/4 pl-10">
+      <div className="text-2xl font-semibold text-gold leading-tight text-right">
+        Ganesha
       </div>
-    </header>
-  )
-}
+    </div>
 
-export default NavbarUser
+    {/* Középső: Ganesha logó – szélesebb */}
+    <div className="basis-1/2 flex justify-center">
+      <img
+        src={ganeshaLogo}
+        alt="Ganesha logo"
+        className="w-48 h-48 object-contain"
+      />
+    </div>
+
+    {/* Jobb oldal: linkek */}
+    <div className="basis-1/4 pr-10 flex justify-end">
+      <NavLinksUser />
+    </div>
+  </div>
+	);
+};
+
+export default NavbarUser;
