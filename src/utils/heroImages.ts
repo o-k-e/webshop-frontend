00b84bcd4a2a3dcb0f1b6c6c0bad31e819b427a8
o-k-e <123556323+o-k-e@users.noreply.github.com>
2025-08-11 18:56:44 +0200

    refactor: add new images to HeroCarousel

--- src/utils/heroImages.ts
+++ src/utils/heroImages.ts
@@ -2,6 +2,7 @@ export const heroImages = [
     'src/assets/malas-textile-bg.png',
     'src/assets/hanging-malas-textiles.jpeg',
     'src/assets/silver-malas-hanging-malas.jpeg',
-    'src/assets/copper-plate-flower-vessel.jpeg',
+    'src/assets/ganesha-sumaranis-incents.jpeg',
+    'src/assets/copper-plate-flower.jpeg',
     'src/assets/copper-statues.jpeg'
 ]
