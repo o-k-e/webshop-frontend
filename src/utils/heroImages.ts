db6b0a8a2d451f48b061266ecdea982cb274b335
o-k-e <123556323+o-k-e@users.noreply.github.com>
2025-08-30 17:13:18 +0200

    refactor: update heroImages.ts file to be able to find images for deployment on render.com

--- src/utils/heroImages.ts
+++ src/utils/heroImages.ts
@@ -1,8 +1,15 @@
+import malasTextileBg from "../assets/malas-textile-bg.png";
+import hangingMalasTextiles from "../assets/hanging-malas-textiles.jpeg";
+import silverMalasHangingMalas from "../assets/silver-malas-hanging-malas.jpeg";
+import ganeshaSumaranisIncents from "../assets/ganesha-sumaranis-incents.jpeg";
+import copperPlateFlower from "../assets/copper-plate-flower.jpeg";
+import copperStatues from "../assets/copper-statues.jpeg";
+
 export const heroImages = [
-    'src/assets/malas-textile-bg.png',
-    'src/assets/hanging-malas-textiles.jpeg',
-    'src/assets/silver-malas-hanging-malas.jpeg',
-    'src/assets/ganesha-sumaranis-incents.jpeg',
-    'src/assets/copper-plate-flower.jpeg',
-    'src/assets/copper-statues.jpeg'
+    malasTextileBg,
+    hangingMalasTextiles,
+    silverMalasHangingMalas,
+    ganeshaSumaranisIncents,
+    copperPlateFlower,
+    copperStatues
 ]
