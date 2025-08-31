d43b53b98bc3737fe3c378b6859fa894e74a2397
o-k-e <123556323+o-k-e@users.noreply.github.com>
2025-08-31 08:52:29 +0200

    refactor: update HeroImages.ts

--- src/utils/heroImages.ts
+++ src/utils/heroImages.ts
@@ -1,8 +1,51 @@
+import malasWithTextileBg from "../assets/malas-with-textile-bg.jpg";
+import textiles1 from "../assets/textiles1.jpg";
+import parfumOil1 from "../assets/parfum-oil1.jpg";
+import malasHanging from "../assets/malas-hanging.jpg";
+import sumanarisInBoxes from "../assets/sumanaris-in-boxes.jpg";
+import textiles2 from "../assets/textiles2.jpg";
+import silverMalasPendants3 from "../assets/silver-malas-pendants3.jpg";
+import silverMalas3 from "../assets/silver-malas3.jpg";
+import silverPendants from "../assets/silver-pendants.jpg";
+// import incents from "../assets/incents6.jpg";
+// import ganeshaAndOtherStatues from "../assets/ganesha-and-other-statues.jpg";
+// import ganeshaShop from "../assets/ganesha-shop.jpg";
+// import ganeshaShopStrilky from "../assets/ganesha-shop-strilky.jpg";
+// import goldenTray from "../assets/golden-tray.jpg";
+// import medalsOnBox from "../assets/medals-on-box.jpg";
+// import nandiGaneshas from "../assets/nandi-ganeshas.jpg";
+// import rezkannaNetikanna from "../assets/rezkanna-netikanna.jpg";
+// import smallGaneshas from "../assets/small-ganeshas.jpg";
+
 export const heroImages = [
-    'src/assets/malas-textile-bg.png',
-    'src/assets/hanging-malas-textiles.jpeg',
-    'src/assets/silver-malas-hanging-malas.jpeg',
-    'src/assets/ganesha-sumaranis-incents.jpeg',
-    'src/assets/copper-plate-flower.jpeg',
-    'src/assets/copper-statues.jpeg'
-]
+    malasWithTextileBg,
+    textiles1,
+    parfumOil1,
+
+    malasHanging,
+    sumanarisInBoxes,
+    malasWithTextileBg,
+
+    textiles1,
+    textiles2,
+    parfumOil1,
+
+    silverMalas3,
+    silverMalasPendants3,
+    silverPendants,
+
+    // nandiGaneshas,
+    // medalsOnBox,
+    // ganeshaShop,
+
+    // ganeshaAndOtherStatues,
+    // incents,
+    // rezkannaNetikanna,
+
+    // smallGaneshas,
+    // goldenTray,
+    // ganeshaShopStrilky,
+
+
+
+];
