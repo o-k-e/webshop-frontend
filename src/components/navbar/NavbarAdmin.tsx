40ed7957a74acc6a0f016a326ae0dba14ce5e6bd
o-k-e <123556323+o-k-e@users.noreply.github.com>
2025-09-01 11:49:30 +0200

    refactor: update minor design elements

--- src/components/navbar/NavbarAdmin.tsx
+++ src/components/navbar/NavbarAdmin.tsx
@@ -3,30 +3,30 @@ import ganeshaLogo from '../../assets/ganesha-logo-removebg.png';
 import { ADMIN_NAV_LINKS } from '../../constants/navLinks.config';
 
 const NavbarAdmin = () => {
-	return (
-		<nav className="flex items-center h-full w-full">
-			{/* Bal oldal: Ganesha Admin felirat */}
-			<div className="basis-1/4 pl-10">
-				<div className="text-2xl font-semibold text-gold leading-tight text-right whitespace-nowrap">
-					Ganesha Admin
-				</div>
-			</div>
+    return (
+        <nav className="flex items-center h-full w-full">
+            {/* Bal oldal: Ganesha Admin felirat */}
+            {/* <div className="basis-1/4 pl-10">
+                <div className="text-2xl font-semibold text-gold leading-tight text-right whitespace-nowrap">
+                    Ganesha Admin
+                </div>
+            </div> */}
 
-			{/* Közép: Ganesha logó */}
-			<div className="basis-1/2 flex justify-center">
-				<img
-					src={ganeshaLogo}
-					alt="Ganesha logo"
-					className="w-48 h-48 object-contain"
-				/>
-			</div>
+            {/* Közép: Ganesha logó */}
+            <div className="basis-1/4 flex justify-center">
+                <img
+                    src={ganeshaLogo}
+                    alt="Ganesha logo"
+                    className="w-48 h-48 object-contain"
+                />
+            </div>
 
-			{/* Jobb oldal: navlinkek */}
-			<div className="basis-1/4 pr-10 flex justify-end">
-				<NavLinks links={ADMIN_NAV_LINKS} isLoggedIn={true} userRole="admin" />
-			</div>
-		</nav>
-	);
+            {/* Jobb oldal: navlinkek */}
+            <div className="basis-1/4 pr-10 flex justify-end">
+                <NavLinks links={ADMIN_NAV_LINKS} isLoggedIn={true} userRole="admin" />
+            </div>
+        </nav>
+    );
 };
 
 export default NavbarAdmin;
