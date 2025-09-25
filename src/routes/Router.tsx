ab698f0c86ba2780d41ad7c667fd570fa2aaec9c
o-k-e <123556323+o-k-e@users.noreply.github.com>
2025-09-25 16:33:22 +0200

    feat: add routes to Cart and MyProfile components

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,52 +1,56 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import UserLayout from "../layouts/UserLayout";
-import Home from "../pages/Home";
-import ProductDetails from "../pages/ProductDetails";
-import AdminLayout from "../layouts/AdminLayout";
-import AdminDashboard from "../pages/admin/AdminDashboard";
-import AdminProducts from "../pages/admin/AdminProducts";
-import ProductForm from "../pages/admin/ProductForm";
-import { AuthProvider } from "../auth/providers/AuthProvider";
-import RequireAuth from "../auth/routes/RequireAuth";
-import Unauthorized from "../auth/pages/Unauthorized";
-import Login from "../pages/Login";
-import Logout from "../pages/Logout";
-import SearchResultsPage from "../pages/SearchResultsPage";
-import ProductUpdateForm from "../pages/admin/ProductUpdateForm";
-import Users from "../pages/Users";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import UserLayout from '../layouts/UserLayout';
+import Home from '../pages/Home';
+import ProductDetails from '../pages/ProductDetails';
+import AdminLayout from '../layouts/AdminLayout';
+import AdminDashboard from '../pages/admin/AdminDashboard';
+import AdminProducts from '../pages/admin/AdminProducts';
+import ProductForm from '../pages/admin/ProductForm';
+import { AuthProvider } from '../auth/providers/AuthProvider';
+import RequireAuth from '../auth/routes/RequireAuth';
+import Unauthorized from '../auth/pages/Unauthorized';
+import Login from '../pages/Login';
+import Logout from '../pages/Logout';
+import SearchResultsPage from '../pages/SearchResultsPage';
+import ProductUpdateForm from '../pages/admin/ProductUpdateForm';
+import Users from '../pages/Users';
+import { Cart } from '../pages/cart/Cart';
+import { MyProfile } from '../pages/user/MyProfile';
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <UserLayout />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: "search", element: <SearchResultsPage /> },
-      { path: "products/:id", element: <ProductDetails /> },
-    ],
-  },
-  {
-    path: "/admin",
-    element: (
-      <RequireAuth allowedRole={"admin"}>
-        <AdminLayout />
-      </RequireAuth>
-    ),
-    children: [
-      { index: true, element: <AdminDashboard /> },
-      { path: "products", element: <AdminProducts /> },
-      { path: "products/create-product", element: <ProductForm /> },
-      { path: "products/edit/:id", element: <ProductUpdateForm /> },
-      { path: "users", element: <Users />},
-    ],
-  },
-  { path: "/login", element: <Login /> },
-  { path: "/logout", element: <Logout /> },
-  { path: "/unauthorized", element: <Unauthorized /> },
+	{
+		path: '/',
+		element: <UserLayout />,
+		children: [
+			{ index: true, element: <Home /> },
+			{ path: 'search', element: <SearchResultsPage /> },
+			{ path: 'products/:id', element: <ProductDetails /> },
+			{ path: '/cart', element: <Cart /> },
+      { path: '/profile', element: <MyProfile />}
+		],
+	},
+	{
+		path: '/admin',
+		element: (
+			<RequireAuth allowedRole={'admin'}>
+				<AdminLayout />
+			</RequireAuth>
+		),
+		children: [
+			{ index: true, element: <AdminDashboard /> },
+			{ path: 'products', element: <AdminProducts /> },
+			{ path: 'products/create-product', element: <ProductForm /> },
+			{ path: 'products/edit/:id', element: <ProductUpdateForm /> },
+			{ path: 'users', element: <Users /> },
+		],
+	},
+	{ path: '/login', element: <Login /> },
+	{ path: '/logout', element: <Logout /> },
+	{ path: '/unauthorized', element: <Unauthorized /> },
 ]);
 
 export const Router = () => (
-  <AuthProvider>
-    <RouterProvider router={router} />
-  </AuthProvider>
+	<AuthProvider>
+		<RouterProvider router={router} />
+	</AuthProvider>
 );
